fc1ccd1c25d995da2fb20b3cbf05ab14f17742fd
Adam Brush <brushat@gmail.com>
2012-11-30 22:52:41 -0500

    Matrix Tests Working: 1,2,8,9
    addEdge working and I think I've figured out all of the tests that I
    need to do with the nodes.  8,9 say they test weight(...) but it's wrong
    so I'm going to tackle that next

--- brushat_HW05_2/MatrixGraph.cpp
+++ brushat_HW05_2/MatrixGraph.cpp
@@ -9,27 +9,27 @@
 #include "MatrixGraph.h"
 
 MatrixGraph::MatrixGraph(unsigned num_nodes){
-	// http://au.answers.yahoo.com/question/index?qid=20081125114819AA8swQ5
+	// Got some help here: http://au.answers.yahoo.com/question/index?qid=20081125114819AA8swQ5
 	// Read number of vertices and number of edges
 	num_edges = 0;
 	// create m*n vector matrix full of zeroes (AKA 'M')
 	M.resize(num_nodes);
 	// it's a vector of vectors so...
-	// loop through num nodes
+	// loop through num nodes (n and m are equal to the number of nodes
 	for(int m = 0; m < M.size(); m++){
 		// for each node loop through num nodes again and put a 0 at [m][n]
 		for(int n = 0; n < M.size(); n++){
 			M.at(m).push_back(0);
 		}
-		// results in...
+		// if num nodes = 5, results in...
+		// http://www.personal.psu.edu/kes32/MichiganClasses/math217/Worksheets2/adjacency-Graphs.pdf
 		// 0 0 0 0 0
 		// 0 0 0 0 0
 		// 0 0 0 0 0
 		// 0 0 0 0 0
 		// 0 0 0 0 0
-		// rows and columns are determined by the number of nodes
-		// since edges go both directions
-		// "x is A from y" also means "y is A from x"
+		// rows and columns are determined by the number of nodes, but rows = cols ALWAYS
+		
 	}
 	// edgeList.resize(num_nodes)
 	
@@ -38,10 +38,32 @@ MatrixGraph::MatrixGraph(unsigned num_nodes){
 }
 
 MatrixGraph::~MatrixGraph(){
-
+	// Do mothiing here, M was not created with 'new' so it should not be deleted
 }
 
+/**
+* Adds an edge between two nodes based on the weight of the edge
+* Note: for the purpose of this program the EdgeWeight is the distance between starbucks locs
+*/
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
+	// Checks we need to do...
+	// Check: u and v are in the right range... you will always need to check this with each node
+	if((0 <= u < M.size()) && (0 <= v < M.size())){
+		// Check: we are passing in a weight > 0
+		if(weight > 0){
+			// Check: u does not equal v
+			if((u != v) && (v != u)){
+				// Check: there is no weight already at that edge
+				if((M.at(u).at(v) == 0) && (M.at(v).at(u) == 0)){
+					// since edges go both directions in our problem
+					// "x is 100 from y" also means "y is 100 from x"
+					M.at(u).at(v) = weight; // Credit to Scott Vincent for showing me how to access M[u][v]
+					M.at(v).at(u) = weight;
+					num_edges++;
+				}
+			}
+		}
+	}
 
 }
 
@@ -54,9 +76,11 @@ EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
 * Credit to Dr. Brinkman for some of this code which he gave out in lecture
 */
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
-	std::list<NWPair> ret;
-
-	return ret;
+	if(0 <= u < M.size()){
+		std::list<NWPair> ret;
+		return ret;
+	}
+	
 }
 
 unsigned MatrixGraph::degree(NodeID u) const{
